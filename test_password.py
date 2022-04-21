24d6fd07332dff13ecd91092f3fdd9f966d11568
Grace Nyutu <graceynyutu@gmail.com>
2022-04-21 16:03:59 +0300

    test on Credentials class to see if objects(username,password) are instantiated correctly

--- test_password.py
+++ test_password.py
@@ -9,5 +9,13 @@ class TestUser(unittest.TestCase):
     def test_init(self):
         self.assertTrue(self.new_user.account,"Twitter")
 
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        self.new_credentials = Credentials("gracenyutu", "frozen56")
+    
+    def test_init(self):
+        self.assertTrue(self.new_credentials.username,"gracenyutu")
+        self.assertTrue(self.new_credentials.password,"frozen56")
+        
 if __name__ == '__main__':
     unittest.main()
