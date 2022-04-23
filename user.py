b842cc852cde12c2e75af1c4d37441363f0015c9
Grace Nyutu <graceynyutu@gmail.com>
2022-04-23 23:22:05 +0300

    create new method to find credentials using the site_name eg. facebook site

--- user.py
+++ user.py
@@ -46,11 +46,33 @@ class Credentials:
         '''
         Credentials.credentials_list.append(self)
 
+    def test_find_credentials_by_site_name(self):
+        '''
+        test to check if we can find the credentials by site_name and display information
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("tiktok", "celeb", "tok@#en")
+        test_credentials.save_credentials()
+
+        found_credentials = Credentials.find_by_site_name("tiktok")
+
+        self.assertEqual(found_credentials.site_name,test_credentials.site_name)
     def delete_credentials(self):
         '''
-        delete_credentials method deletes a saved user
+        delete_credentials method deletes saved credentials
         '''
         Credentials.credentials_list.remove(self)
+
+    def test_copy_email(self):
+        '''
+        Test to confirm that we are copying the email address from a found contact
+        '''
+
+        self.new_credentials.save_credentials()
+        Credentials.copy_email("0712345678")
+
+        self.assertEqual(self.new_contact.email,pyperclip.paste())
 
     @classmethod
     def display_credentials(cls):
