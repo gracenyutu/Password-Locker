72b189bd398569567fecef61c25c81a290e2ad0b
Grace Nyutu <graceynyutu@gmail.com>
2022-04-23 21:27:55 +0300

    create functions that call save and delete methods on the user and credentials

--- password.py
+++ password.py
@@ -13,3 +13,27 @@ def create_credentials(site_name, username, password):
     '''
     new_credentials = Credentials(site_name, username, password)
     return new_credentials
+
+def save_users(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+
+def save_cred(credentials):
+    '''
+    Function to save credential
+    '''
+    credentials.save_credentials()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+def del_credentials(credentials):
+    '''
+    Function to delete a credential
+    '''
+    credentials.delete_credentials()
